92bf5b02f27f21606750d24fdf939934905d455d
jonasnoliveira <jonasnoliveirabr@gmail.com>
2024-05-20 21:40:18 -0300

    update

---
+++ Migrations/20240520184049_updatePropertiesStudents.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ApiCrud.Migrations
+{
+    /// <inheritdoc />
+    public partial class updatePropertiesStudents : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Address",
+                table: "Students",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Cpf",
+                table: "Students",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Students",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Phone",
+                table: "Students",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Address",
+                table: "Students");
+
+            migrationBuilder.DropColumn(
+                name: "Cpf",
+                table: "Students");
+
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Students");
+
+            migrationBuilder.DropColumn(
+                name: "Phone",
+                table: "Students");
+        }
+    }
+}
